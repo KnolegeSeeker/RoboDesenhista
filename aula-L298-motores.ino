7dfd29e2e86a0fa934babc7f841a8e06f061b120
KnolegeSeeker <KnolegeSeeker@users.noreply.github.com>
2016-09-20 20:47:04 -0300

    Add files via upload

--- aula-L298-motores.ino
+++ aula-L298-motores.ino
@@ -1,7 +1,15 @@
-#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <Wire.h>
 
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+int pinod0 = 2;
+int rpm;
+volatile byte pulsos;
+//unsigned long timeold;
+
+unsigned int ppv = 6; //pulsos por volta
+
 const int ena = 10;
 const int p1ma = 8;
 const int p2ma = 7;
@@ -10,7 +18,19 @@ const int enb = 9;
 const int p1mb = 6;
 const int p2mb = 5;
 
+void contador ()
+{
+  pulsos--;
+  
+}
+
 void setup() {
+  Serial.begin(9600);
+  pinMode(pinod0, INPUT);
+  attachInterrupt(digitalPinToInterrupt(pinod0), contador, CHANGE);
+  pulsos = 0;
+  rpm = 0;
+  //timeold = 0;
   lcd.begin(16, 2);
   pinMode(ena, OUTPUT);
   pinMode(p1ma, OUTPUT);
@@ -23,16 +43,24 @@ void setup() {
 }
 
 void loop() {
-  digitalWrite(ena, LOW);
-  digitalWrite(enb, LOW);
 
-  RotacionaMotor(1, 1);
-  RotacionaMotor(2, 2);
-
-  digitalWrite(enb, HIGH);
-  digitalWrite(ena, HIGH);
-  delay(2000);
-
+  
+  
+  
+  do {
+    digitalWrite(ena, LOW);
+    digitalWrite(enb, LOW);
+  
+    RotacionaMotor(1, 1);
+    RotacionaMotor(2, 2);
+  
+    digitalWrite(enb, HIGH);
+    digitalWrite(ena, HIGH);
+    delay(150);
+    Serial.println(pulsos, DEC);
+  } while (pulsos > 0);
+  //delay(2000);
+/*
   digitalWrite(ena, LOW);
   digitalWrite(enb, LOW);
 
@@ -49,8 +77,16 @@ void loop() {
   lcd.setCursor(0,1);
   lcd.print("teste2");
   delay(1000);
-  lcd.setBacklight(LOW);
-  delay(1000);
+
+    rpm = pulsos/(millis() * 1000);
+    pulsos = 0;
+
+    Serial.print("pulsos = ");
+    Serial.println(pulsos, DEC);
+    Serial.print("Hz = ");
+    Serial.println(rpm, DEC);
+*/
+    //attachInterrupt(0, contador, FALLING);
 
 }
 
